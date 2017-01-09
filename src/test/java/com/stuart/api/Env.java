bfadd7c5c7ab79ad843cfc7676fa5ca6d365e230
Rafa Visa <r.visa@stuart.com>
2017-01-09 15:30:59 +0100

    Add QA environment by default

--- src/test/java/com/stuart/api/Env.java
+++ src/test/java/com/stuart/api/Env.java
@@ -11,6 +11,7 @@ public class Env {
     private static final String CLIENT_ID_KEY = "clientId";
     private static final String CLIENT_API_ID_KEY = "clientApiId";
     private static final String CLIENT_API_SECRET_KEY = "clientSecret";
+
     private static final String API_URI_SANDBOX = "http://sandbox-api.stuart.com";
     private static final String USERNAME_SANDBOX = "r.visa@stuart.com";
     private static final String PASSWORD_SANDBOX = "test1234";
@@ -19,6 +20,16 @@ public class Env {
             "81_87aa5725abf59ee5b8fbbf84540786c8a5eff6c89475dbd2f7";
     private static final String CLIENT_API_SECRET_SANDBOX =
             "5667150462818adad05dbbc0b41fe512fd1e4b692d1111dffe";
+
+    private static final String API_URI_QA = "http://qa-api.stuart.com";
+    private static final String USERNAME_QA = "r.visa@stuart.com";
+    private static final String PASSWORD_QA = "test1234";
+    private static final int CLIENT_ID_QA = 39888;
+    private static final String CLIENT_API_ID_QA =
+            "18e8707235651b7648bb48c647976bb922c05411c57f828e4d64164e44e12331";
+    private static final String CLIENT_API_SECRET_QA =
+            "1c672b969a43f46ab0a2b8b0cfb1128750d863296cfba91fe255c3932b7a4052";
+
     private static final String API_URI_BETA = "http://beta-api.stuart.com";
     private static final String USERNAME_BETA = "r.visa@stuart.com";
     private static final String PASSWORD_BETA = "test1234";
@@ -34,7 +45,7 @@ public class Env {
         // Run with -Denvironment=beta
         String environmentProperty = System.getProperty("environment");
         // Sandbox by default
-        String apiEnvironment = environmentProperty == null ? "sandbox" : environmentProperty;
+        String apiEnvironment = environmentProperty == null ? "qa" : environmentProperty;
         switch (apiEnvironment) {
             case "sandbox":
                 env.put(API_URI_KEY, API_URI_SANDBOX);
@@ -43,6 +54,14 @@ public class Env {
                 env.put(CLIENT_ID_KEY, String.valueOf(CLIENT_ID_SANDBOX));
                 env.put(CLIENT_API_ID_KEY, CLIENT_API_ID_SANDBOX);
                 env.put(CLIENT_API_SECRET_KEY, CLIENT_API_SECRET_SANDBOX);
+                break;
+            case "qa":
+                env.put(API_URI_KEY, API_URI_QA);
+                env.put(USERNAME_KEY, USERNAME_QA);
+                env.put(PASSWORD_KEY, PASSWORD_QA);
+                env.put(CLIENT_ID_KEY, String.valueOf(CLIENT_ID_QA));
+                env.put(CLIENT_API_ID_KEY, CLIENT_API_ID_QA);
+                env.put(CLIENT_API_SECRET_KEY, CLIENT_API_SECRET_QA);
                 break;
             case "beta":
                 env.put(API_URI_KEY, API_URI_BETA);
